760b4f9353ab654c3676ca5c270cb6a0a48326cf
Elijah Lofgren <elijahlofgren@gmail.com>
2018-02-25 21:36:39 -0600

    Start to list historical markers using example at  https://alligator.io/vuejs/rest-api-axios/

--- code/src/pages/index.vue
+++ code/src/pages/index.vue
@@ -1,14 +1,55 @@
 <template>
   <q-page class="flex flex-center">
+   
+  <ul v-if="posts && posts.length">
+    <li v-for="post of posts">
+      <p><strong>{{post.title}}</strong></p>
+      <p>{{post.County}}</p>
+    </li>
+  </ul>
+
+  <ul v-if="errors && errors.length">
+    <li v-for="error of errors">
+      {{error.message}}
+    </li>
+  </ul>
+   test
     <img src="~assets/quasar-logo-full.svg">
   </q-page>
+
 </template>
 
-<style>
-</style>
-
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  created() {
+    axios.get(`http://13.82.106.207/?q=mobileapi/markersjson.json`)
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
 }
 </script>
